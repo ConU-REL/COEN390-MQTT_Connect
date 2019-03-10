026a8a938ba29895d4baee7ff1d10ad609f58399
MalihehSharafi <malihehsharafi@gmail.com>
2019-03-10 13:35:21 -0400

    Publish feature added.

--- app/src/main/java/com/example/mqttconnect/MainActivity.java
+++ app/src/main/java/com/example/mqttconnect/MainActivity.java
@@ -12,11 +12,22 @@ import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity {
-
+     /*
+     to connect with password:
+     static String MQTTHOST=
+     static String USERNAME=
+     static String PASSWORD=
+     String topicStr=
+      */
     private Button connection_button;
     private Button disconnect_button;
+    private Button publish_button;
+    MqttAndroidClient client;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,15 +35,21 @@ public class MainActivity extends AppCompatActivity {
         connection_button=findViewById(R.id.connection_button);
         String clientId = MqttClient.generateClientId();
         //the server url must be replaced with the designated one
-        final MqttAndroidClient client =
+        client =
                 new MqttAndroidClient(MainActivity.this, "tcp://broker.hivemq.com:1883",
                         clientId);
+        //MqttConnectOptions options = new MqttConnectOptions();
+        //options.setUserName("USERNAME");
+        //options.setPassword("PASSWORD".toCharArray());
+
+
         connection_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
 
                 try {
+                    //IMqttToken token = client.connect(options);
                     IMqttToken token = client.connect();
                     token.setActionCallback(new IMqttActionListener() {
                         @Override
@@ -80,5 +97,20 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+    }
+    public void toPublish(View view)
+    {
+        publish_button=findViewById(R.id.publish_button);
+        String topic = "foo/bar";
+        String message = "Hello World";
+        //byte[] encodedPayload = new byte[0];
+        try {
+            //encodedPayload = message.getBytes("UTF-8");
+            //MqttMessage message = new MqttMessage(encodedPayload);
+            client.publish(topic, message.getBytes(),0,false);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 }
